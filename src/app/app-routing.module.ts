3102f6fa840f9b138b7c8eb3e031763d2555f8fc
OverPa1n <vasilishin08@gmail.com>
2021-07-28 14:34:13 +0300

    add home-page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { VacanciesEditComponent } from './vacancies/vacancies-edit/vacancies-edi
 import { VacanciesComponent } from './vacancies/vacancies.component';
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {QuestionsComponent} from "./questions-library/questions/questions.component";
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {path: '', component: SignInComponent},
+  {path: 'home', component: HomePageComponent},
   {path:'questions',component: QuestionsComponent},
   {path: 'vacancies', component: VacanciesComponent},
   {path: 'vacancies-create',component: VacanciesCreateComponent},
