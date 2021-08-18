588c5237dddbe6569f1000ecc0974bf4fd0c649c
Yurii <tyrpuchy0611@gmail.com>
2021-08-18 12:09:21 +0300

    fixed outlook of set-reviewer modal

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -4,13 +4,13 @@ import {Injectable} from '@angular/core';
 export class Constants {
   public modalWidth = {
     xs: '490px',
-    s: '510px',
+    s: '450px',
     m: '760px'
   };
   public modalHeight = {
     m: '500px',
     l: '850px',
-    s: '305px'
+    s: '200px'
   };
 
 }
